5e1a68e6c0b4d00b6196a96b00306966d8e6b777
Rabbi <jasrabbi50@gmail.com>
2022-06-28 00:21:07 +0600

    reorder linked list

---
+++ reorder_linked_list.py
@@ -0,0 +1,56 @@
+# You are given the head of a singly linked-list. The list can be represented as:
+
+# L0 → L1 → … → Ln - 1 → Ln
+# Reorder the list to be on the following form:
+
+# L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+# You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4]
+# Output: [1,4,2,3]
+# Example 2:
+
+
+# Input: head = [1,2,3,4,5]
+# Output: [1,5,2,4,3]
+
+# first solution
+# time: O(n)
+# space: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # middle linked list
+        slow, first = head, head.next
+        while first and first.next:
+            slow = slow.next
+            first = first.next.next
+        # reverge second list
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+            
+        # merge two halfs
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
+
