4fa60cc34d74c4249206912a29ae1dc5bceacf2d
rabbi50 <jasrabbi50@gmail.com>
2022-07-03 12:50:32 +0600

    solve final value of variable after performing operation

---
+++ final_value_of_variable_after_performing_operation.py
@@ -0,0 +1,64 @@
+# There is a programming language with only four operations and one variable X:
+
+# ++X and X++ increments the value of the variable X by 1.
+# --X and X-- decrements the value of the variable X by 1.
+# Initially, the value of X is 0.
+
+# Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+ 
+
+# Example 1:
+
+# Input: operations = ["--X","X++","X++"]
+# Output: 1
+# Explanation: The operations are performed as follows:
+# Initially, X = 0.
+# --X: X is decremented by 1, X =  0 - 1 = -1.
+# X++: X is incremented by 1, X = -1 + 1 =  0.
+# X++: X is incremented by 1, X =  0 + 1 =  1.
+# Example 2:
+
+# Input: operations = ["++X","++X","X++"]
+# Output: 3
+# Explanation: The operations are performed as follows:
+# Initially, X = 0.
+# ++X: X is incremented by 1, X = 0 + 1 = 1.
+# ++X: X is incremented by 1, X = 1 + 1 = 2.
+# X++: X is incremented by 1, X = 2 + 1 = 3.
+# Example 3:
+
+# Input: operations = ["X++","++X","--X","X--"]
+# Output: 0
+# Explanation: The operations are performed as follows:
+# Initially, X = 0.
+# X++: X is incremented by 1, X = 0 + 1 = 1.
+# ++X: X is incremented by 1, X = 1 + 1 = 2.
+# --X: X is decremented by 1, X = 2 - 1 = 1.
+# X--: X is decremented by 1, X = 1 - 1 = 0.
+ 
+
+# Constraints:
+
+# 1 <= operations.length <= 100
+# operations[i] will be either "++X", "X++", "--X", or "X--".
+
+# first solution
+# time: O(n)
+# space: O(1)
+
+from this import s
+
+
+class Solution:
+    def finalValueAfterOperations(self, operations) -> int:
+        X = 0
+        for operation in operations:
+            if operation in ["--X", "X--"]:
+                X-=1
+            elif operation in ["++X","X++"]:
+                X+=1
+        return X
+        
+sol = Solution()
+print(sol.finalValueAfterOperations(["--X","X++","X++"]))
