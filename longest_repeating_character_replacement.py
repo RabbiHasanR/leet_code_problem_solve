199f4322dd7757394a6b7983befa4a9028c88c52
rabbi50 <jasrabbi50@gmail.com>
2022-07-19 17:17:38 +0600

    jewels_and_stones problem solved

--- longest_repeating_character_replacement.py
+++ longest_repeating_character_replacement.py
@@ -27,14 +27,33 @@
 # time: O(n)
 # space: O(n)
 
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+#         count = {}
+#         res = 0
+#         l = 0
+#         for r in range(len(s)):
+#             count[s[r]] = 1 + count.get(s[r],0)
+#             if (r-l+1) - max(count.values()) > k:
+#                 count[s[l]] -= 1
+#                 l += 1
+#             res = max(res, r-l+1)
+#         return res
+
+# second solution
+# time: O(n)
+# space: O(n)
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
         res = 0
         l = 0
+        maxf = 0
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r],0)
-            if (r-l+1) - max(count.values()) > k:
+            maxf = max(maxf, count[s[r]])
+            if (r-l+1) - maxf > k:
                 count[s[l]] -= 1
                 l += 1
             res = max(res, r-l+1)
