c11687bce4a9983b0653701d9fb12eb66d6ec195
rabbi50 <jasrabbi50@gmail.com>
2022-08-08 11:09:08 +0600

    solve palindromic substring problem

---
+++ string/palingromic_substring.py
@@ -0,0 +1,55 @@
+# Given a string s, return the number of palindromic substrings in it.
+
+# A string is a palindrome when it reads the same backward as forward.
+
+# A substring is a contiguous sequence of characters within the string.
+
+ 
+
+# Example 1:
+
+# Input: s = "abc"
+# Output: 3
+# Explanation: Three palindromic strings: "a", "b", "c".
+# Example 2:
+
+# Input: s = "aaa"
+# Output: 6
+# Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consists of lowercase English letters.
+
+
+# first solution
+# time: O(n)
+# space: O(1)
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        
+        for i in range(len(s)):
+            #for odd length
+            l, r = i, i
+            
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+            
+            # for even length
+            l, r = i, i+1
+            
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+        return res
+
+sol = Solution()
+s = "abc"
+print(sol.countSubstrings(s))
