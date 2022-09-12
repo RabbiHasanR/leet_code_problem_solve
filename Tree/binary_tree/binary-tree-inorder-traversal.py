d3236fa6a4d888f5135c586b1dd2ec6e71c39332
rabbi50 <jasrabbi50@gmail.com>
2022-09-12 10:19:18 +0600

    binary tree problem preorder and inorder traversal problem solve

---
+++ Tree/binary_tree/binary-tree-inorder-traversal.py
@@ -0,0 +1,52 @@
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+# Example 2:
+
+# Input: root = []
+# Output: []
+# Example 3:
+
+# Input: root = [1]
+# Output: [1]
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+ 
+
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+
+# first solution using recursion
+# time: O(n)
+# space: O(n)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorder(self,node,l):
+        if node.left:
+            self.inorder(node.left,l)
+        l.append(node.val)
+        if node.right:
+            self.inorder(node.right,l)
+            
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        l = []
+        if root:
+            self.inorder(root,l)
+        return l
+        
---
+++ Tree/binary_tree/binary-tree-preorder-traversal.py
@@ -0,0 +1,51 @@
+# Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [1,null,2,3]
+# Output: [1,2,3]
+# Example 2:
+
+# Input: root = []
+# Output: []
+# Example 3:
+
+# Input: root = [1]
+# Output: [1]
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+ 
+
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+
+# first solution with recursivly
+# time: O(n)
+# space: O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pre_order(self,node,res):
+        res.append(node.val)
+        if node.left:
+            self.pre_order(node.left,res)
+        if node.right:
+            self.pre_order(node.right,res)
+            
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if root:
+            self.pre_order(root,res)
+        return res
+        
