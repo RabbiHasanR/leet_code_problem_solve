61484ec06b3ce0d74ea976fbf5d237737931f7f5
rabbi50 <jasrabbi50@gmail.com>
2022-09-15 16:12:48 +0600

    palindrome linked list problem solve with O(1) space

--- linked_list/palindrome-linked-list.py
+++ linked_list/palindrome-linked-list.py
@@ -39,3 +39,37 @@ class Solution:
             values.append(curr.val)
             curr = curr.next
         return values == values[::-1]
+
+# second solution (find middle of linked list,reverse second part of linked list and using two pointer to compare value)
+# time: O(n)
+# space: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # find mid linekd list
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # reverse part 2 linked list
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+            
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+               
