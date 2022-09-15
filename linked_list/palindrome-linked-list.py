376972ae28f86c6dcf647faa971e2fb2eb508cb7
rabbi50 <jasrabbi50@gmail.com>
2022-09-15 16:02:35 +0600

    palindrome linked list problem solve

---
+++ linked_list/palindrome-linked-list.py
@@ -0,0 +1,41 @@
+# Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,2,1]
+# Output: true
+# Example 2:
+
+
+# Input: head = [1,2]
+# Output: false
+ 
+
+# Constraints:
+
+# The number of nodes in the list is in the range [1, 105].
+# 0 <= Node.val <= 9
+ 
+
+# Follow up: Could you do it in O(n) time and O(1) space?
+
+# first solution
+# tiem: O(n)
+# space: O(n)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        values = []
+        curr = head
+        while curr:
+            values.append(curr.val)
+            curr = curr.next
+        return values == values[::-1]
